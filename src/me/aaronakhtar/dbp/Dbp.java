8e21370b8ad2a9156d9210011568d4376f505c60
Aaron Akhtar <52239843+Aaron-Akhtar@users.noreply.github.com>
2020-09-21 22:58:44 +0100

    init

---
+++ src/me/aaronakhtar/dbp/Dbp.java
@@ -0,0 +1,25 @@
+package me.aaronakhtar.dbp;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Dbp {
+
+    protected static final File target = new File("./imesh.txt");
+    protected static final File output = new File("./output.txt");
+
+    public static void main(String[] args) {
+        String s;
+        try(BufferedReader reader = new BufferedReader(new FileReader(target))){
+            int x = 0;
+            while((s = reader.readLine()) != null){
+                x++;
+                new Thread(new DbpRunn(s, x)).start();
+            }
+        }catch (Exception e){
+
+        }
+    }
+
+}
---
+++ src/me/aaronakhtar/dbp/DbpRunn.java
@@ -0,0 +1,30 @@
+package me.aaronakhtar.dbp;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class DbpRunn implements Runnable{
+
+    private static void write(String s){
+        try(PrintWriter writer = new PrintWriter(new FileWriter(Dbp.output, true), true)){
+            writer.write(s);
+        }catch (Exception e){
+
+        }
+    }
+
+    private String line;
+    private int num;
+
+    public DbpRunn(String line, int num) {
+        this.line = line;
+        this.num = num;
+    }
+
+    @Override
+    public void run() {
+        String[] line = this.line.split(":");
+        write(line[1] + ":" + line[3] + ":" + line[2] + "\n");
+        System.out.println(line[1] + ":" + line[3] + ":" + line[2] + " -        ["+num+"] \n");
+    }
+}
